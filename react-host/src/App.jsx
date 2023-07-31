be28165414f594b010edd29261533a8264b2b286
GAMELOFT\dan.luongcong <dan.luongcong@gameloft.com>
2023-07-31 15:01:42 +0700

    + Counter Wrapper for react

--- react-host/src/App.jsx
+++ react-host/src/App.jsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 import "./index.scss";
 
-const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: react-host</div>
-    <div>Framework: react</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Tailwind</div>
-  </div>
-);
+import CounterWrapper from "remote/CounterWrapper";
+
+const App = () =>
+{
+  const divRef = useRef(null);
+
+  useEffect(() => {
+    CounterWrapper(divRef.current);
+  }, []);
+
+  return (
+    <div className="mt-10 text-3xl mx-auto max-w-6xl">
+      <div>Name: react-host</div>
+      <div ref={divRef}></div>
+    </div>
+  )
+};
 ReactDOM.render(<App />, document.getElementById("app"));
